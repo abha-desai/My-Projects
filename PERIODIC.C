2e9bfe22462d612e94daaf236592f90dec3d2477
Aabha D <desaiabha3011@gmail.com>
2016-12-13 13:53:32 -0500

    Update PERIODIC.C

--- PERIODIC.C
+++ PERIODIC.C
@@ -1,3 +1,7 @@
+//The Modern Periodic Table project is a simple console application built without the use of graphics. 
+//It is developed using the C programming language for the purpose of storing name, symbol, atomic number, 
+//atomic weight, and some important properties as well as to display them as per requirement of the user.
+
 #include<stdio.h>
 #include<conio.h>
 #include<string.h>
